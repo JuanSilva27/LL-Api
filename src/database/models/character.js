2c6eac168bda42d3a7fe5ed15422853ed8095a89
Juan Silva <juan.silva.0270@gmail.com>
2024-02-27 10:50:16 -0300

    se detallan mas los de la tabla character

--- src/database/models/character.js
+++ src/database/models/character.js
@@ -4,28 +4,67 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Character extends Model {
-    
-    static associate({Group,Subunit,Seiyuu,Charactersong}) {
-      this.belongsTo(Group,{foreignKey: "groupId"});
-      this.belongsTo(Subunit,{foreignKey: "subunitId"});
-      this.belongsTo(Seiyuu,{foreignKey: "seiyuuId"});
-      this.belongsToMany(Charactersong,{foreignKey: "id_character"});
+
+    static associate({ Group, Subunit, Seiyuu, Charactersong }) {
+      this.belongsTo(Group, { foreignKey: "groupId" });
+      this.belongsTo(Subunit, { foreignKey: "subunitId" });
+      this.belongsTo(Seiyuu, { foreignKey: "seiyuuId" });
+      this.belongsToMany(Charactersong, { foreignKey: "id_character" });
     }
   }
   Character.init({
-    name: DataTypes.STRING,
-    birthday: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-    height: DataTypes.DECIMAL,
-    sizes: DataTypes.STRING,
-    groupId: DataTypes.INTEGER,
-    subunitId: DataTypes.INTEGER,
-    color: DataTypes.STRING,
-    signature: DataTypes.STRING,
-    photo: DataTypes.STRING,
-    seiyuuId: DataTypes.INTEGER,
-    description: DataTypes.STRING,
-    icon: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    birthday: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    sizes: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    subunitId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    signature: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    seiyuuId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    icon: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
   }, {
     sequelize,
     modelName: 'Character',
