06a1bec8ad15abcb1f540ed20f4f5110e8404e4e
Juan Silva <juan.silva.0270@gmail.com>
2024-03-01 11:23:49 -0300

    se cambia el tipo de dato, para podear aceptar descripciones mas largas

--- src/database/models/seiyuu.js
+++ src/database/models/seiyuu.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: true
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     photo: {
